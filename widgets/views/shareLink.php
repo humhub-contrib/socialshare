a79e997e6d9c61b409a1718de91522ef8fb211e3
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-12-21 22:04:01 -0500

    Fix: Description not appearing in text area

--- widgets/views/shareLink.php
+++ widgets/views/shareLink.php
@@ -28,6 +28,6 @@ use yii\helpers\Html;
 	    <?= Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($permalink) . '&description=' . urlencode($object->getContentDescription()),['onclick'=> $option]);?>
 	    <?= Html::a('<i class="fa fa-twitter" style="font-size:16px;color:#55acee">&nbsp;</i>&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink),['onclick'=> $option]);?>
 	    <?= Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5">&nbsp;</i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink) . '&ro=false', ['onclick'=> $option]);?>
-	    <?= Html::a('<i class="fa fa-share" style="font-size:16px;color:#00c300"></i>', 'https://social-plugins.line.me/lineit/share?url=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink),['onclick'=> $option]);?>
+	    <?= Html::a('<i class="fa fa-share" style="font-size:16px;color:#00c300"></i>', 'https://social-plugins.line.me/lineit/share?' . '&text=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink),['onclick'=> $option]);?>
 	</div>
 </span>

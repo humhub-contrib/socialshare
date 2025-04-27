7d0a417d9eddff9ffa061ff269b318dfa0a9379c
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-04-27 06:37:03 -0400

    Update Module.php

--- Module.php
+++ Module.php
@@ -2,7 +2,14 @@
 
 namespace humhub\modules\socialshare;
 
+use yii\helpers\Url;
+
 class Module extends \humhub\components\Module
 {
     public $resourcesPath = 'resources';
+
+    public function getConfigUrl()
+    {
+        return Url::to(['/socialshare/admin']);
+    }
 }

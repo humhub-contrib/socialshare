3215eb4720edc5037433a17c97b1d46c65172e56
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-04-29 18:26:40 -0400

    Enh: Use `foreach` with new `getEnabledPlatforms()` method

--- widgets/views/shareLink.php
+++ widgets/views/shareLink.php
@@ -2,12 +2,9 @@
 
 use yii\helpers\Html;
 use humhub\modules\socialshare\assets\Assets;
-use humhub\modules\socialshare\models\ConfigureForm;
 use humhub\modules\socialshare\services\SocialShareService;
 
 Assets::register($this);
-
-$settings = new ConfigureForm();
 
 $linkOptions = [
     'target' => '_blank',
@@ -20,50 +17,12 @@ $socialShareService = new SocialShareService();
 ?>
 
 <?= Html::beginTag('span', $options) ?>
-    <div class="pull-right">
-        <?php if ($settings->facebook_enabled): ?>
-            <?= $socialShareService->createShareLink(
-                SocialShareService::PLATFORM_FACEBOOK,
-                $permalink,
-                $object->getContentDescription(),
-                $linkOptions
-            ); ?>
-        <?php endif; ?>
-
-        <?php if ($settings->twitter_enabled): ?>
-            <?= $socialShareService->createShareLink(
-                SocialShareService::PLATFORM_TWITTER,
-                $permalink,
-                $object->getContentDescription(),
-                $linkOptions
-            ); ?>
-        <?php endif; ?>
-
-        <?php if ($settings->linkedin_enabled): ?>
-            <?= $socialShareService->createShareLink(
-                SocialShareService::PLATFORM_LINKEDIN,
-                $permalink,
-                $object->getContentDescription(),
-                $linkOptions
-            ); ?>
-        <?php endif; ?>
-
-        <?php if ($settings->line_enabled): ?>
-            <?= $socialShareService->createShareLink(
-                SocialShareService::PLATFORM_LINE,
-                $permalink,
-                $object->getContentDescription(),
-                $linkOptions
-            ); ?>
-        <?php endif; ?>
-
-        <?php if ($settings->bluesky_enabled): ?>
-            <?= $socialShareService->createShareLink(
-                SocialShareService::PLATFORM_BLUESKY,
-                $permalink,
-                $object->getContentDescription(),
-                $linkOptions
-            ); ?>
-        <?php endif; ?>
-    </div>
+<?php foreach ($socialShareService->getEnabledPlatforms() as $platform) : ?>
+    <?= $socialShareService->createShareLink(
+        $platform,
+        $permalink,
+        $object->getContentDescription(),
+        $linkOptions
+    ); ?>
+<?php endforeach; ?>
 <?= Html::endTag('span') ?>

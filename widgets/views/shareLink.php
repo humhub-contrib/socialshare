dc9e71e21a0937181f37102ce5be22d0966bf5be
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-04-30 03:32:40 -0400

    Update shareLink.php

--- widgets/views/shareLink.php
+++ widgets/views/shareLink.php
@@ -17,12 +17,5 @@ $socialShareService = new SocialShareService();
 ?>
 
 <?= Html::beginTag('span', $options) ?>
-<?php foreach ($socialShareService->getEnabledPlatforms() as $platform) : ?>
-    <?= $socialShareService->createShareLink(
-        $platform,
-        $permalink,
-        $object->getContentDescription(),
-        $linkOptions
-    ); ?>
-<?php endforeach; ?>
+    <?= $socialShareService->renderShareLinks($object, $permalink, $linkOptions) ?>
 <?= Html::endTag('span') ?>

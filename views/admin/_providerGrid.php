839debc0f9cb216875eeed0228c446081792aab7
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2026-01-08 19:40:57 -0500

    Chg: Browser confirmation to HH native confirmation

--- views/admin/_providerGrid.php
+++ views/admin/_providerGrid.php
@@ -5,6 +5,7 @@ use humhub\modules\ui\icon\widgets\Icon;
 use humhub\modules\admin\grid\CheckboxColumn;
 use humhub\widgets\bootstrap\Button;
 use humhub\widgets\GridView;
+use humhub\widgets\form\ActiveForm;
 use yii\data\ArrayDataProvider;
 use yii\grid\ActionColumn;
 use yii\helpers\Url;
@@ -64,14 +65,33 @@ use yii\helpers\Url;
                 'view' => function (): void {
                     return;
                 },
-                'delete' => fn($url, $model) =>
-                    !$model->is_default 
-                        ? Button::danger()->icon('trash')->link(Url::to(['delete', 'id' => $model->id]))->sm()
-                            ->options([
-                                'data-method' => 'post',
-                                'data-confirm' => Yii::t('SocialshareModule.base', 'Are you sure you want to delete this provider?'),
-                            ])
-                        : '',
+                'delete' => function($url, $model) {
+                    if ($model->is_default) {
+                        return '';
+                    }
+                    
+                    $formId = 'delete-form-' . $model->id;
+                    ob_start();
+                    $form = ActiveForm::begin([
+                        'id' => $formId,
+                        'action' => Url::to(['delete', 'id' => $model->id]),
+                        'method' => 'post',
+                        'acknowledge' => true,
+                        'options' => ['style' => 'display: inline;']
+                    ]);
+                    echo Button::danger()
+                        ->icon('trash')
+                        ->sm()
+                        ->submit()
+                        ->confirm(
+                            Yii::t('SocialshareModule.base', 'Delete Provider'),
+                            Yii::t('SocialshareModule.base', 'Are you sure you want to delete this provider?'),
+                            Yii::t('base', 'Delete'),
+                            Yii::t('base', 'Cancel')
+                        );
+                    ActiveForm::end();
+                    return ob_get_clean();
+                },
                 'update' => fn($url, $model) =>
                     Button::primary()->icon('edit')->link(Url::to(['edit', 'id' => $model->id]))->sm(),
             ],
